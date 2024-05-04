d7c9984751f1459c5cf05ebbf43383e02cef2eee
Sami Brahim Djelloul <61477348+samuaws@users.noreply.github.com>
2024-05-04 17:37:38 +0200

    fIX

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,4 @@
+using FischlWorks_FogWar;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public GameObject eye;
     public GameObject player;
+    public csFogWar csfow;
+
 
     private void Awake()
     {
@@ -19,7 +22,8 @@ public class GameManager : MonoBehaviour
     IEnumerator SpawnLight()
     {
         yield return new WaitForSeconds(1);
-        Instantiate(eye,player.transform.position,Quaternion.identity);
+        var fr = Instantiate(eye,player.transform.position,Quaternion.identity);
+        csfow.AddFogRevealer(new csFogWar.FogRevealer(fr.transform, 10, true));
         StartCoroutine(SpawnLight());
     }
 }
